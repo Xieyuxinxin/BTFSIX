0dcc3d386d7c36429588d191797666cffe741bc7
张帅帅 <1327879272@qq.com>
2021-05-31 21:16:49 +0800

    Update DiscussionGroupMain.java

--- 张帅帅/pages/DiscussionGroupMain.java
+++ 张帅帅/pages/DiscussionGroupMain.java
@@ -18,22 +18,24 @@ public class DiscussionGroupMain {
 	//页面对象
 	//讨论组
 	public AndroidElement group() {
-		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"讨论组 讨论组 第 2 个标签，共 4 个\"]"));
+//		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"讨论组 讨论组 第 2 个标签，共 4 个\"]"));
+		return driver.findElementByAccessibilityId("讨论组\n"+"讨论组\n"+"第2个标签，供4个");
 	}
 	
 	//创建聊天
 	public AndroidElement add_group() {
-		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"创建聊天\"]"));
+//		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"创建聊天\"]"));
+		return driver.findElementByAccessibilityId("创建聊天");
 	}
 	
 	//讨论组test
 	public AndroidElement group_test() {
-		return driver.findElementByAccessibilityId("test");
+		return driver.findElementByAccessibilityId("群\n"+"test");
 	}
 	
 	//系统讨论组
 	public AndroidElement group_system() {
-		return driver.findElementByAccessibilityId("系统");
+		return driver.findElementByAccessibilityId("系统\n"+"所有人");
 	}
 
 }
