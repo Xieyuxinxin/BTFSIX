2481d722ddaf1efdb79cb575a04138365b987ee8
张帅帅 <1327879272@qq.com>
2021-05-31 21:17:26 +0800

    Update DiscussionGroupSelectNumber.java

--- 张帅帅/pages/DiscussionGroupSelectNumber.java
+++ 张帅帅/pages/DiscussionGroupSelectNumber.java
@@ -19,19 +19,23 @@ public class DiscussionGroupSelectNumber {
 	//联系人
 	//admin
 	public AndroidElement admin() {
-		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"A admin\"]"));
+//		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"A admin\"]"));
+		return driver.findElementByAccessibilityId("A\n"+"admin");
 	}
 	//test2
 	public AndroidElement test2() {
-		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"T test2\"]"));
+//		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"T test2\"]"));
+		return driver.findElementByAccessibilityId("T\n"+"test2");
 	}
 	//monkeytest
 	public AndroidElement monkeytest() {
-		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"M monkeytest\"]"));
+//		return driver.findElement(By.xpath("//android.view.View[@content-desc=\"M monkeytest\"]"));
+		return driver.findElementByAccessibilityId("M\n"+"monkeytest");
 	}
 	//返回键
 	public AndroidElement button_return() {
-		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"返回\"]"));
+//		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"返回\"]"));
+		return driver.findElementByAccessibilityId("返回");
 	}
 	//确定键
 	public AndroidElement button_confirm() {
@@ -43,11 +47,13 @@ public class DiscussionGroupSelectNumber {
 	}
 	//取消
 	public AndroidElement button_cancle() {
-		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"取消\"]"));
+//		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"取消\"]"));
+		return driver.findElementByAccessibilityId("取消");
 	}
-	//确定
+	//创建
 	public AndroidElement button_create() {
-		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"创建\"]"));
+//		return driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"创建\"]"));
+		return driver.findElementByAccessibilityId("创建");
 	}
 	
 	//页面方法
